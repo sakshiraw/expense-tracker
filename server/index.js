4563b176d541b9d4e48d1bb8c8490564e6065bfa
Sakshi Rawat <sakraw.2004@gmail.com>
2026-06-14 13:04:31 +0530

    Fix Render port configuration

--- server/index.js
+++ server/index.js
@@ -53,7 +53,8 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
